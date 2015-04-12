ad5befd078089c1c59d81e6fecb6ecbe80714605
Emily Zhao <emily@dropbox.com>
2015-04-12 13:41:27 -0700

    clear drawing on each attempt

--- app/src/main/java/com/herokuapp/ezhao/circle/CircleDrawingView.java
+++ app/src/main/java/com/herokuapp/ezhao/circle/CircleDrawingView.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class CircleDrawingView extends View {
     private Paint paint;
     private Path path;
+    private ArrayList<Point> points;
 
     public CircleDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -19,14 +23,14 @@ public class CircleDrawingView extends View {
         paint.setAntiAlias(true);
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.STROKE);
-
-        path = new Path();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(path, paint);
+        if (path != null) {
+            canvas.drawPath(path, paint);
+        }
     }
 
     @Override
@@ -36,10 +40,26 @@ public class CircleDrawingView extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                path = new Path();
+                points = new ArrayList<>();
+
                 path.moveTo(pointX, pointY);
+                points.add(new Point(Math.round(pointX), Math.round(pointY)));
+
                 return true;
             case MotionEvent.ACTION_MOVE:
                 path.lineTo(pointX, pointY);
+                points.add(new Point(Math.round(pointX), Math.round(pointY)));
+                break;
+            case MotionEvent.ACTION_UP:
+                path.lineTo(pointX, pointY);
+                points.add(new Point(Math.round(pointX), Math.round(pointY)));
+
+                // Close the loop, TODO(emily) consider removing
+                if (points.size() > 2) {
+                    Point firstPoint = points.get(0);
+                    path.lineTo(firstPoint.x, firstPoint.y);
+                }
                 break;
             default:
                 return true;
