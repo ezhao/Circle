c928e2d396478e8b8d59f2037bf5e4031f02b403
Emily Zhao <emily@dropbox.com>
2015-04-12 14:39:26 -0700

    calculate perfect circle

--- app/src/main/java/com/herokuapp/ezhao/circle/CircleDrawingView.java
+++ app/src/main/java/com/herokuapp/ezhao/circle/CircleDrawingView.java
@@ -2,6 +2,7 @@ package com.herokuapp.ezhao.circle;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
@@ -13,8 +14,12 @@ import java.util.ArrayList;
 
 public class CircleDrawingView extends View {
     private Paint paint;
+    private Paint guidePaint;
     private Path path;
     private ArrayList<Point> points;
+    private float centerX;
+    private float centerY;
+    private float radius;
 
     public CircleDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,6 +28,12 @@ public class CircleDrawingView extends View {
         paint.setAntiAlias(true);
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.STROKE);
+
+        guidePaint = new Paint();
+        guidePaint.setAntiAlias(true);
+        guidePaint.setStrokeWidth(1);
+        guidePaint.setStyle(Paint.Style.STROKE);
+        guidePaint.setColor(Color.BLUE);
     }
 
     @Override
@@ -30,6 +41,9 @@ public class CircleDrawingView extends View {
         super.onDraw(canvas);
         if (path != null) {
             canvas.drawPath(path, paint);
+        }
+        if (radius > 0) {
+            canvas.drawCircle(centerX, centerY, radius, guidePaint);
         }
     }
 
@@ -46,6 +60,7 @@ public class CircleDrawingView extends View {
                 path.moveTo(pointX, pointY);
                 points.add(new Point(Math.round(pointX), Math.round(pointY)));
 
+                radius = 0;
                 return true;
             case MotionEvent.ACTION_MOVE:
                 path.lineTo(pointX, pointY);
@@ -55,11 +70,7 @@ public class CircleDrawingView extends View {
                 path.lineTo(pointX, pointY);
                 points.add(new Point(Math.round(pointX), Math.round(pointY)));
 
-                // Close the loop, TODO(emily) consider removing
-                if (points.size() > 2) {
-                    Point firstPoint = points.get(0);
-                    path.lineTo(firstPoint.x, firstPoint.y);
-                }
+                calculateCircle();
                 break;
             default:
                 return true;
@@ -67,5 +78,37 @@ public class CircleDrawingView extends View {
 
         postInvalidate();
         return true;
+    }
+
+    private void calculateCircle() {
+        int totalX = 0;
+        int totalY = 0;
+        int totalArea = 0;
+        Point point, nextPoint;
+        int x, nextX, y, nextY;
+        for (int i = 0; i < points.size()-1; i++) {
+            point = points.get(i);
+            x = point.x;
+            y = point.y;
+
+            nextPoint = points.get(i+1);
+            nextX = nextPoint.x;
+            nextY = nextPoint.y;
+
+            totalX += (x + nextX) * (x * nextY - nextX * y);
+            totalY += (y + nextY) * (x * nextY - nextX * y);
+
+            totalArea += (x * nextY - nextX * y);
+        }
+        float area = 1.0f * totalArea / 2;
+        centerX = 1.0f / 6.0f * totalX / area;
+        centerY = 1.0f / 6.0f * totalY / area;
+
+        float totalDistance = 0;
+        for (int i = 0; i < points.size()-1; i++) {
+            point = points.get(i);
+            totalDistance += Math.sqrt(Math.pow((point.x - centerX), 2) + Math.pow((point.y - centerY), 2));
+        }
+        radius = totalDistance/points.size();
     }
 }
